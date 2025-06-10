2be42c8d5871bfe77cf15ebd26a3d9ed9efe6a71
Michiel Eijpe <mail@mteij.nl>
2025-06-10 23:17:46 +0200

    make it less strict.

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -14,6 +14,20 @@ module.exports = {
     "no-restricted-globals": ["error", "name", "length"],
     "prefer-arrow-callback": "error",
     "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    // BEGIN NEW ESLINT RULES FOR FLEXIBILITY
+    "max-len": ["error", {
+      "code": 120, // Relax maximum line length to 120 characters
+      "ignoreComments": true, // Ignore comments from max-len check
+      "ignoreUrls": true, // Ignore URLs from max-len check
+      "ignoreStrings": true, // Ignore long strings from max-len check
+      "ignoreTemplateLiterals": true, // Ignore template literals from max-len check
+      "ignoreRegExpLiterals": true // Ignore regex literals from max-len check
+    }],
+    "indent": ["error", 2, { // Enforce 2-space indentation consistently
+      "SwitchCase": 1 // Keep 1-level indent for SwitchCase
+    }],
+    "no-trailing-spaces": "off", // Turn off the rule for trailing spaces
+    // END NEW ESLINT RULES
   },
   overrides: [
     {
