6d14609c672d9a141447cc9e4212ecc88c1be00d
Michiel Eijpe <mail@mteij.nl>
2025-06-11 14:46:48 +0200

    PWA

---
+++ public/sw.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'schikko-rules-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/js/main.js',
+  '/js/firebase.js',
+  '/js/ui.js',
+  '/randomizer/randomizer.js',
+  '/randomizer/randomizer.css',
+  '/assets/favicon.png',
+  'https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700&family=MedievalSharp&display=swap',
+  'https://cdn.tailwindcss.com'
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
